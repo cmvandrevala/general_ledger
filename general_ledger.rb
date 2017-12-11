e80afcf9fd09e2c8310bc385a5b653081a60a618
Cyrus Vandrevala <cyrus.vandrevala@cupcakephysics.com>
2017-12-11 08:01:11 -0600

    Extract initialization of Api class

--- general_ledger.rb
+++ general_ledger.rb
@@ -3,20 +3,21 @@ require_relative 'lib/main/api'
 
 class GeneralLedger < Sinatra::Base
   before do
+    @api = Api.new
     request.body.rewind
     @request_payload = JSON.parse request.body.read
   end
 
   post '/open_account' do
-    Api.new.open_account(@request_payload)
+    @api.open_account(@request_payload)
   end
 
   post '/close_account' do
-    Api.new.close_account(@request_payload)
+    @api.close_account(@request_payload)
   end
 
   post '/append_snapshot' do
-    Api.new.append_snapshot(@request_payload)
+    @api.append_snapshot(@request_payload)
   end
 
   run! if app_file == $0
